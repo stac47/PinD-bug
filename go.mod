05d6e10a86b38c862c68811da41cd9b66c534c60
Laurent Stacul <laurent.stacul@amadeus.com>
2021-12-10 19:16:17 +0000

    Another bug

--- go.mod
+++ go.mod
@@ -3,16 +3,15 @@ module github.com/stac47/docker-bug
 go 1.15
 
 require (
-	github.com/containerd/containerd v1.4.1 // indirect
-	github.com/containerd/continuity v0.0.0-20200928162600-f2cc35102c2a // indirect
+	github.com/Microsoft/go-winio v0.5.1 // indirect
+	github.com/containerd/containerd v1.5.8 // indirect
 	github.com/docker/distribution v2.7.1+incompatible // indirect
-	github.com/docker/docker v17.12.0-ce-rc1.0.20200916142827-bd33bbf0497b+incompatible
+	github.com/docker/docker v20.10.11+incompatible
 	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/docker/go-units v0.4.0 // indirect
-	github.com/gogo/protobuf v1.3.1 // indirect
-	github.com/opencontainers/image-spec v1.0.1 // indirect
-	github.com/opencontainers/runc v0.1.1 // indirect
-	github.com/sirupsen/logrus v1.7.0 // indirect
-	golang.org/x/sys v0.0.0-20201008064518-c1f3e3309c71 // indirect
-	google.golang.org/grpc v1.32.0 // indirect
+	github.com/opencontainers/image-spec v1.0.2 // indirect
+	golang.org/x/net v0.0.0-20211209124913-491a49abca63 // indirect
+	golang.org/x/sys v0.0.0-20211210111614-af8b64212486 // indirect
+	google.golang.org/genproto v0.0.0-20211208223120-3a66f561d7aa // indirect
+	google.golang.org/grpc v1.42.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,34 +2,53 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"io"
+	"os"
 
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
-	"github.com/docker/docker/pkg/archive"
+	"github.com/docker/docker/pkg/stdcopy"
 )
 
 func main() {
-	cli, err := client.NewClientWithOpts(client.FromEnv)
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
 		panic(err)
 	}
 
-	buildOptions := types.ImageBuildOptions{
-		Tags:       []string{"test-docker-bug"},
-		Dockerfile: "Dockerfile",
-		// Remove:      true,
-		// ForceRemove: true,
-	}
-
-	dockerContext, err := archive.TarWithOptions("./data", &archive.TarOptions{})
+	reader, err := cli.ImagePull(ctx, "docker.io/library/alpine", types.ImagePullOptions{})
 	if err != nil {
 		panic(err)
 	}
-	_, err = cli.ImageBuild(context.TODO(), dockerContext, buildOptions)
-	if err == nil {
-		fmt.Println("Should have returned an error")
-	} else {
+	io.Copy(os.Stdout, reader)
+
+	resp, err := cli.ContainerCreate(ctx, &container.Config{
+		Image: "alpine",
+		Cmd:   []string{"echo", "hello world"},
+	}, nil, nil, nil, "")
+	if err != nil {
 		panic(err)
 	}
+
+	if err := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+		panic(err)
+	}
+
+	statusCh, errCh := cli.ContainerWait(ctx, resp.ID, container.WaitConditionNotRunning)
+	select {
+	case err := <-errCh:
+		if err != nil {
+			panic(err)
+		}
+	case <-statusCh:
+	}
+
+	out, err := cli.ContainerLogs(ctx, resp.ID, types.ContainerLogsOptions{ShowStdout: true})
+	if err != nil {
+		panic(err)
+	}
+
+	stdcopy.StdCopy(os.Stdout, os.Stderr, out)
 }
