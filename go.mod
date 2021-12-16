2f99a8969f1b55f756f0d5bec925a8813a6ac7ca
Laurent Stacul <laurent.stacul@amadeus.com>
2021-12-16 16:21:25 +0000

    From context tarball

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/docker/docker v20.10.11+incompatible
 	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/docker/go-units v0.4.0 // indirect
+	github.com/morikuni/aec v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.0.2 // indirect
 	golang.org/x/net v0.0.0-20211209124913-491a49abca63 // indirect
 	golang.org/x/sys v0.0.0-20211210111614-af8b64212486 // indirect
--- main.go
+++ main.go
@@ -2,21 +2,78 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"io"
+	"log"
 	"os"
+	"regexp"
+	"strings"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/jsonmessage"
 	"github.com/docker/docker/pkg/stdcopy"
 )
 
+var sha256Pattern = regexp.MustCompile("^[0-9a-z]+$")
+
+func findImageIDFromStream(str string) string {
+	str = strings.TrimSpace(str)
+	if sha256Pattern.MatchString(str) {
+		return str
+	}
+	return ""
+}
+
+func parseDockerDaemonJsonMessages(r io.Reader) (string, error) {
+	var decoder *json.Decoder
+	decoder = json.NewDecoder(r)
+	imageID := ""
+	for {
+		var jsonMessage jsonmessage.JSONMessage
+		if err := decoder.Decode(&jsonMessage); err != nil {
+			if err == io.EOF {
+				break
+			}
+			return "", err
+		}
+		if err := jsonMessage.Error; err != nil {
+			return "", err
+		}
+		if jsonMessage.Aux != nil {
+			var r types.BuildResult
+			if err := json.Unmarshal(*jsonMessage.Aux, &r); err != nil {
+			} else {
+				imageID = r.ID
+			}
+		}
+		// Hack for podman <= 4.0.0
+		if imageID == "" {
+			imageID = findImageIDFromStream(jsonMessage.Stream)
+		}
+	}
+	return strings.TrimPrefix(imageID, "sha256:"), nil
+}
+
 func main() {
-	imageId := os.Args[1]
+	contextTarPath := os.Args[1]
 	ctx := context.Background()
 	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
 		panic(err)
 	}
+	dockerBuildContext, err := os.Open(contextTarPath)
+	defer dockerBuildContext.Close()
+	buildOptions := types.ImageBuildOptions{
+		Dockerfile: "Dockerfile",
+	}
+	buildResponse, err := cli.ImageBuild(ctx, dockerBuildContext, buildOptions)
+	imageId, err := parseDockerDaemonJsonMessages(buildResponse.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer buildResponse.Body.Close()
 
 	resp, err := cli.ContainerCreate(ctx, &container.Config{
 		Image: imageId,
